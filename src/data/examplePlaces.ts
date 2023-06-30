9698eae9d58da9dd6a6428ba3fc50e94d3fe7ce2
nbogie <neillbogie@googlemail.com>
2023-06-30 22:52:49 +0100

    formatting only

--- src/data/examplePlaces.ts
+++ src/data/examplePlaces.ts
@@ -15,16 +15,20 @@ export const examplePlaces: Place[] = [
         title: "The best bit of Hampstead Heath",
         placeName: "Golders Hill Park",
         country: "London",
-        description: "A very pretty park with rolling hills (and deer), on London's northern line.",
-        imageURL: "https://images.unsplash.com/photo-1591752509219-b097a7b5697c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1934&q=80",
+        description:
+            "A very pretty park with rolling hills (and deer), on London's northern line.",
+        imageURL:
+            "https://images.unsplash.com/photo-1591752509219-b097a7b5697c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1934&q=80",
         mapURL: "https://goo.gl/maps/Skf5hCTruz4WFAAm9",
     },
     {
         title: "White and blue",
         placeName: "Vilankulos",
         country: "Mozambique",
-        description: "Surreal white sand, blue-green water, empty blue skies, and quiet.",
-        imageURL: "https://media-cdn.tripadvisor.com/media/vr-splice-j/00/c5/dd/13.jpg",
+        description:
+            "Surreal white sand, blue-green water, empty blue skies, and quiet.",
+        imageURL:
+            "https://media-cdn.tripadvisor.com/media/vr-splice-j/00/c5/dd/13.jpg",
         mapURL: "https://goo.gl/maps/jfTejuobgNeWSbq98",
-    }
+    },
 ];
