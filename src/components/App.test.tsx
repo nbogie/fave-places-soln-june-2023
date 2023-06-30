78cc3243aeb10f25d50e2a0a5af2a0933f94e1a9
nbogie <neillbogie@googlemail.com>
2023-06-30 23:48:47 +0100

    correct label of app component test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -3,7 +3,7 @@ import App from "./App";
 
 //An example of using react-testing-library
 describe("App component", async () => {
-    test("Should have text Hello, World on it", () => {
+    test("Should have text 'Fave places' on it", () => {
         render(<App />);
         const elem = screen.getByText("Fave places");
         expect(elem).toBeInTheDocument();
